bea6a107e8dcdfd065998f47052eaafd4f913af5
Gozel Yazmuhamedova <gozelyazmuhamedowa@gmail.com>
2022-06-13 11:22:08 -0700

    sidebar v1

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    <button>asdas</button>
+  <div class="sidebar">
+    <a id="logo" href="#"><img class="logo" src="@/assets/img/logo.svg" alt="logo" /></a>
+
+    <div class="nav">
+      <a href="#"><img class="home" src="@/assets/img/home.svg" alt="home" />Home page</a>
+      <a href="#"><img class="home" src="@/assets/img/discover.svg" alt="discover" />Discover</a>
+      <a href="#"><img class="home" src="@/assets/img/notifications.svg" alt="notifications" />Notifications</a>
+      <a href="#"><img class="home" src="@/assets/img/messages.svg" alt="messages" />Messages</a>
+      <a href="#"><img class="home" src="@/assets/img/placemarks.svg" alt="placemarks" />Place marks</a>
+      <a href="#"><img class="home" src="@/assets/img/lists.svg" alt="lists" />lists</a>
+       <a href="#"><img class="home" src="@/assets/img/profile.svg" alt="profile" />Profile</a>
+      <a href="#"><img class="home" src="@/assets/img/more.svg" alt="more" />More</a>
+    </div>
+
+    <div class="button">tweet</div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="scss">
-@import "~/assets/scss/components/sidebar";
+@import '~/assets/scss/components/sidebar';
 </style>
